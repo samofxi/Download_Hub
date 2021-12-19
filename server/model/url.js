e2aa6c0ee55d4936cb89c07a3a03db73b1af28b4
Salman <78206653+samofxi@users.noreply.github.com>
2021-12-19 11:16:42 +0100

    Revert "Download hub"
    This reverts commit 9514b055f84c4bda413247021937f8c81378aaef.

--- server/model/url.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const urlSchema = new mongoose.Schema({
-  longUrl: Array,
-  FileName: Array,
-  date: { type: String, default: Date.now }
-});
-
-module.exports = mongoose.model('Url', urlSchema);
