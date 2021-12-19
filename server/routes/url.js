626d5d86ff13eef45db1151d4a9f40264651f83a
Salman <78206653+samofxi@users.noreply.github.com>
2021-12-19 13:21:05 +0100

    Update url.js

--- server/routes/url.js
+++ server/routes/url.js
@@ -11,34 +11,32 @@ route.post('/shorten', async (req, res) => {
     const { longUrl } = req.body;
     const { FileName } = req.body;
     const baseUrl = process.env.baseUrl;
-  
-    // Check base url
-    if (!validUrl.isUri(baseUrl)) {
-      return res.status(401).json('Invalid base url');
-    }
-  
-    // Create url code
+    if(!validUrl.isUri){
+      res.status(500).send({message: `the current base Url ${baseUrl} is invalid!`})
+  };
 
-    // Check long url
-    if (true) {
-      try{
-          let url = new Url({
-            longUrl,
-            FileName,
-            date: new Date()
-          });
-  
-          await url.save();
-  
-        //res.send(data);
-        res.send({url: baseUrl+"/api/download?id=" + data._id})
-      } catch (err) {
-        console.error(err);
-        res.status(500).json('Server error');
-      }
-    } else {
-      res.status(401).json('Invalid long url');
-    }
+  try{
+      let url = new Url({
+        longUrl,
+        FileName,
+        date: new Date()
+      });
+
+      url
+  .save(url)
+  .then(data => {
+      //res.send(data);
+      res.send({url: baseUrl+"/api/download?id=" + data._id})
+  }) 
+  .catch( err => { 
+      res.status(500).send({
+          message: err.message || "Something went wrong while creating the short Link"
+      });
+  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Server error');
+  }
   });
   
   module.exports = route;
