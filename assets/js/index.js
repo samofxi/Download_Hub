911dea16158b7b96d4e7ded7344020c6be46a615
Salman <78206653+samofxi@users.noreply.github.com>
2021-12-19 14:45:23 +0100

    update js

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,7 @@ function download_files(files,filename) {
         download_next(i + 1);
       }, 500);
       if(files.length == i ){
-        location.replace('https://www.javascripttutorial.net/');
+        setTimeout(() => {  location.reload(); }, 5000);;
       }
     }
     // Initiate the first download.
